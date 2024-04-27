2d36fcfa8166d7b3621bc9f750bf894de4261eab
blue031415 <blue031415@gmail.com>
2024-04-27 18:27:53 +0900

    change layout

---
+++ components/MX-Tsukuba.vue
@@ -0,0 +1,11 @@
+<template>
+    <H1 class = "top">MX-Tsukuba</H1>
+</template>
+
+<style>
+.top{
+    background-color: #fdfdfd;
+    text-align: center;
+    font-size: 3rem;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <MXTsukuba />
+    <dev>
+        <slot/>
+    </dev>
+</template>
