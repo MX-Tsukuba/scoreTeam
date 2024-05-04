980e146f53a35f81c15d92dac4ce48f75f10f03f
kake <Makky.rrr.omach@yahoo.ne.jp>
2024-05-04 18:21:26 +0900

    scoreDisplay-withoutTrueDataset

--- components/scoreInput/index.vue
+++ components/scoreInput/index.vue
@@ -1,20 +1,6 @@
 <!-- 以前のコードほぼそのままなので上手く調節してください -->
 <template>
   <EnterGolfCourseName/>
-  <div>
-
-    <h2>{{ playdata.course }} course's Scores</h2>
-    <ul v-if = "t_sample_kazuki && t_sample_kazuki.length">
-      <li v-for = "sample in t_sample_kazuki" :key="sample.id">
-        <p>H{{ sample.hole || "-" }}, par</p>
-        <div>
-          score: {{ sample.score }}, putts: {{ sample.putts }}
-          <button @click="deletePlayData(sample.id)">x</button>
-        </div> 
-      </li>    
-    </ul>
-    <p v-else>Loading..</p>
-
     <div>
       <h3>Input</h3>
       <form @submit="addPlayData">
@@ -24,8 +10,6 @@
         <button type="submit">Add play data</button>
       </form>
     </div>
-    
-  </div>
 </template>
 
 <script setup lang="ts">
@@ -52,17 +36,6 @@ const addPlayData = async() => {
   if(error){
     alert(error.message);
   }else{
-    return true;
-  }
-};
-
-//データ削除
-const deletePlayData = async(tableId: number)=> {
-  const { error } = await supabase.from('t_sample_kazuki').delete().match({id: tableId});
-  if(error){
-    alert(error.message);
-  }else if(confirm("Delete this data.")){
-    location.reload();
     return true;
   }
 };
